2fc1677ad12b751d2d7ee93d6acecfe0c3d2417c
jongyeol <pjr159@gmail.com>
2022-09-06 22:59:36 +0900

    style: header back button
    - back button comp 분리
    - header left, right 선택적 노출

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../lib/colors';
 
+// Todo: svg 컴포넌트 로고로 바꿀 경우 React.ReactNode 로 타입 바꾸기
 interface Props {
-  title?: string; // Todo svg 컴포넌트 로고로 바꿀 경우 React.ReactNode 로 타입 바꾸기
+  title?: string;
+  headerLeft?: React.ReactNode;
+  headerRight?: string;
 }
 
-function Header({ title = 'emetal' }: Props) {
+function Header({ title = 'emetal', headerLeft, headerRight }: Props) {
   return (
     <Block>
+      {headerLeft && <HeaderSide position="left">{headerLeft}</HeaderSide>}
       <Title>{title}</Title>
+      {headerRight && <HeaderSide position="right">right</HeaderSide>}
     </Block>
   );
 }
 
+const HeaderSide = styled.div<{ position: 'left' | 'right' }>`
+  position: absolute;
+  ${(props) => props.position} : 16px;
+  display: flex;
+  align-items: center;
+  right: 16px;
+  top: 0;
+  height: 100%;
+`;
+
 const Block = styled.header`
+  position: relative;
   height: 56px;
   border-bottom: 1px solid ${colors.gray3};
   padding: 0 16px;
---
+++ components/HeaderBackButton.tsx
@@ -0,0 +1,27 @@
+import ArrowLeft from './vectors/ArrowLeft';
+import styled from 'styled-components';
+
+interface Props {
+  onClick?: () => void;
+}
+
+function HeaderBackButton({ onClick }: Props) {
+  return (
+    <IconButton onClick={onClick}>
+      <ArrowLeft />
+    </IconButton>
+  );
+}
+
+// 버튼 클릭 영역을 더 넓게 가져가기 위해 버튼으로 svg 감싸고 padding 과 -margin 으로 더 넓게 영역 설정
+const IconButton = styled.button`
+  background: none;
+  display: flex;
+  border: none;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  margin-left: -8px;
+`;
+
+export default HeaderBackButton;
