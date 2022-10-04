53bd162761c1c6e0bf2f06654e4aaa3f73afb11a
jongyeol <pjr159@gmail.com>
2022-10-05 02:39:03 +0900

    feat: form onsubmit

--- pages/add/index.tsx
+++ pages/add/index.tsx
@@ -2,15 +2,24 @@ import AddTemplate from '@/components/add/AddTemplate';
 import LabelInput from '@/components/common/LabelInput';
 import LabelTextArea from '@/components/common/LableTextArea';
 import TabTamplete from '@/components/templates/TabTemplate';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 function Add() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
   return (
     <TabTamplete>
-      <AddTemplate title="경매" buttonText="다음">
+      <AddTemplate title="경매" buttonText="다음" handleSubmit={handleSubmit} onSubmit={onSubmit}>
         <Group>
-          <LabelInput label="카테고리" />
-          <LabelInput label="서브 카테고리" />
+          <LabelInput label="카테고리" {...register('category', { required: true })} />
+          <LabelInput label="서브 카테고리" {...register('subCategory', { required: true })} />
           <StyledLabelTextArea label="서브 카테고리" />
         </Group>
       </AddTemplate>
