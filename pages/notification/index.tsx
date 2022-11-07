8ca1b5e476062c31102e411eda0576124385c5ff
leo_kim <leo_kim@gccompany.co.kr>
2022-11-07 09:41:24 +0900

    feat : 알림 ui 개선

--- pages/notification/index.tsx
+++ pages/notification/index.tsx
@@ -2,21 +2,55 @@ import TabTamplete from '@/components/templates/TabTemplate';
 import { useGetNotification } from '@/hooks/notification';
 import { media } from '@/lib/media';
 
-import { List } from 'antd';
+import {List, Tag} from 'antd';
 import styled from 'styled-components';
+import {colors} from "@/lib/colors";
+
+const notificationTypeColorMap = {
+    BID_WON: 'blue', // 입찰낙찰
+    BID_FAIL: 'orange', // 낙찰실패
+    MISCARRIED: 'red', // 유찰
+    ACTIONED_OFF: 'sky', // 경매낙찰
+    ADMIN_NOTICE: 'brown' // 관리자공지
+};
+
+const notificationTypeTextMap = {
+    BID_WON: '입찰낙찰',
+    BID_FAIL: '낙찰실패',
+    MISCARRIED: '유찰',
+    ACTIONED_OFF: '경매낙찰',
+    ADMIN_NOTICE: '관리자공지',
+};
+
+const auctionTypeColorMap = {
+    NORMAL: 'green',
+    REVERSE: 'blue',
+};
+
+const auctionTypeTextMap = {
+    NORMAL: '경매',
+    REVERSE: '역경매',
+};
+
+const notificationItem = (notificationType) => {
+    let color = notificationTypeColorMap[notificationType]
+    let text = notificationTypeTextMap[notificationType]
+    return <Tag color={color}> {text}</Tag>
+}
 
 function Notification() {
   const { data } = useGetNotification();
 
   return (
     <StyledTabTamplete>
-      <Content>
+        <Content>
         <List
           itemLayout="horizontal"
           dataSource={data?.content}
           renderItem={(item) => (
             <List.Item>
-              <List.Item.Meta
+                {notificationItem(item?.notificationType)}
+                <List.Item.Meta
                 title={<a>{item?.content}</a>}
                 // description={
                 //   AUCTION_TYPE[item?.auction?.auctionType] +
@@ -26,6 +60,7 @@ function Notification() {
                 //   item?.auction?.hostUser?.account.accountHolder
                 // }
               />
+                <Tag color={colors.gray4}>{item?.createdAt}</Tag>
             </List.Item>
           )}
         />
