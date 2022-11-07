dd2cc90bc9703087e1f5d8f54255b60b62771ad3
jongyeol <pjr159@gmail.com>
2022-11-07 17:09:20 +0900

    fix: 알림 경매 낙찰 생삭 수정

--- pages/notification/index.tsx
+++ pages/notification/index.tsx
@@ -2,55 +2,55 @@ import TabTamplete from '@/components/templates/TabTemplate';
 import { useGetNotification } from '@/hooks/notification';
 import { media } from '@/lib/media';
 
-import {List, Tag} from 'antd';
+import { List, Tag } from 'antd';
 import styled from 'styled-components';
-import {colors} from "@/lib/colors";
+import { colors } from '@/lib/colors';
 
 const notificationTypeColorMap = {
-    BID_WON: 'blue', // 입찰낙찰
-    BID_FAIL: 'orange', // 낙찰실패
-    MISCARRIED: 'red', // 유찰
-    ACTIONED_OFF: 'sky', // 경매낙찰
-    ADMIN_NOTICE: 'brown' // 관리자공지
+  BID_WON: 'blue', // 입찰낙찰
+  BID_FAIL: 'orange', // 낙찰실패
+  MISCARRIED: 'red', // 유찰
+  ACTIONED_OFF: 'pink', // 경매낙찰
+  ADMIN_NOTICE: 'brown', // 관리자공지
 };
 
 const notificationTypeTextMap = {
-    BID_WON: '입찰낙찰',
-    BID_FAIL: '낙찰실패',
-    MISCARRIED: '유찰',
-    ACTIONED_OFF: '경매낙찰',
-    ADMIN_NOTICE: '관리자공지',
+  BID_WON: '입찰낙찰',
+  BID_FAIL: '낙찰실패',
+  MISCARRIED: '유찰',
+  ACTIONED_OFF: '경매낙찰',
+  ADMIN_NOTICE: '관리자공지',
 };
 
 const auctionTypeColorMap = {
-    NORMAL: 'green',
-    REVERSE: 'blue',
+  NORMAL: 'green',
+  REVERSE: 'blue',
 };
 
 const auctionTypeTextMap = {
-    NORMAL: '경매',
-    REVERSE: '역경매',
+  NORMAL: '경매',
+  REVERSE: '역경매',
 };
 
 const notificationItem = (notificationType) => {
-    let color = notificationTypeColorMap[notificationType]
-    let text = notificationTypeTextMap[notificationType]
-    return <Tag color={color}> {text}</Tag>
-}
+  const color = notificationTypeColorMap[notificationType];
+  const text = notificationTypeTextMap[notificationType];
+  return <Tag color={color}> {text}</Tag>;
+};
 
 function Notification() {
   const { data } = useGetNotification();
 
   return (
     <StyledTabTamplete>
-        <Content>
+      <Content>
         <List
           itemLayout="horizontal"
           dataSource={data?.content}
           renderItem={(item) => (
             <List.Item>
-                {notificationItem(item?.notificationType)}
-                <List.Item.Meta
+              {notificationItem(item?.notificationType)}
+              <List.Item.Meta
                 title={<a>{item?.content}</a>}
                 // description={
                 //   AUCTION_TYPE[item?.auction?.auctionType] +
@@ -60,8 +60,8 @@ function Notification() {
                 //   item?.auction?.hostUser?.account.accountHolder
                 // }
               />
-                {/*<Tag color={colors.gray4}>{item?.createdAt}</Tag>*/}
-                <h5>{item?.createdAt}</h5>
+              {/*<Tag color={colors.gray4}>{item?.createdAt}</Tag>*/}
+              <h5>{item?.createdAt}</h5>
             </List.Item>
           )}
         />
