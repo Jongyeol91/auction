b2fa6a35fc07b784fbb64c74da6a65709df62cf5
jongyeol <pjr159@gmail.com>
2022-12-12 16:39:19 +0900

    feat: 헤더 타이틀 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
             as="style"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css"
           />
+          <title>EMETAL</title>
         </Head>
         <body>
           <Main />
